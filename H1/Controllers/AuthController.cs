0f11ec08b46e64da3251860629ab1ccf758dd4f2
gurkirat1802 <85217914+gurkirat1802@users.noreply.github.com>
2024-04-23 21:59:37 +0530

    Update AuthController.cs

--- H1/Controllers/AuthController.cs
+++ H1/Controllers/AuthController.cs
@@ -17,6 +17,16 @@ namespace H1.Controllers
             public string EmailId { get; set; }
             public string Password { get; set; }
             public string PhoneNumber { get; set; }
+        
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }
+            public string Height { get; set; }
+            public string Weight { get; set; }
+            public string Goal { get; set; }
+            public string Level { get; set; }
+            public string Category { get; set; }
         }
         [Route("Signin")]
         [HttpPost]
@@ -56,6 +66,40 @@ namespace H1.Controllers
                         PhoneNumber = model.PhoneNumber
                     };
 
+                    await _mongoService.CreateAsync(user);
+                }
+                else
+                    return BadRequest("login model is not correct");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+        }
+        [Route("SignupPage")]
+        [HttpPost]
+        public async Task<IActionResult> SignupPage([FromBody] LoginModel model)
+        {
+            try
+            {
+                if (model != null)
+                {
+                    var user = new User
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        EmailId = model.EmailId,
+                        PhoneNumber = model.PhoneNumber,
+                        Age = model.Age,
+                        Gender = model.Gender,
+                        Height = model.Height,
+                        Weight = model.Weight,
+                        Goal = model.Goal,
+                        Level = model.Level,
+                        Category = model.Category
+                    };
+        
                     await _mongoService.CreateAsync(user);
                 }
                 else
