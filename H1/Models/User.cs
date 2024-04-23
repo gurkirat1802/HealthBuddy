bb46c91cf44ae8ed83e33000aa512a6ecfefb72e
gurkirat1802 <85217914+gurkirat1802@users.noreply.github.com>
2024-04-23 22:00:13 +0530

    Update User.cs

--- H1/Models/User.cs
+++ H1/Models/User.cs
@@ -13,5 +13,15 @@ namespace H1.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string EmailId { get; set; }
+        public string Gender { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string Goal { get; set; }
+        public string Level { get; set; }
+        public string Category { get; set; }
     }
 }
